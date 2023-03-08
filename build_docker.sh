8b0a0dccc27f54a7a9adb08e320f13f18d00c06e
Paresh Borkar <pareshborkar@gmail.com>
2023-03-08 05:55:34 +0000

    Add support to generate default self-signed certificates

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install github app dependencies
 pip install -r /tmp/requirements.txt
+echo "/opt/tw_github_app/gen_def_cert.sh" >> /usr/local/bin/twigs-update.sh
 echo "/usr/local/bin/uwsgi --ini /opt/tw_github_app/config/uwsgi.ini" >> /usr/local/bin/twigs-update.sh
 mv /usr/local/bin/twigs-update.sh /usr/local/bin/update-twigs-run-uwsgi.sh
 # Cleanup /tmp
